8776c04acd6b28cae75b511e6922110704413c9b
takaexe <164596411+takaexe@users.noreply.github.com>
2024-03-23 17:10:40 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
 	helpCmdPerPage: 10, //- Number of commands per page of help command
 	lyricsMaxResults: 5, //- Number of results for lyrics command (Do not touch this value if you don't know what you are doing)
-	adminId: "325399199363497985", //- Replace UserId with the Discord ID of the admin of the bot
-	token: process.env.token || "NzMyMzA0OTk5OTU4NDQ2MTEx.GPMVtX.Nxq_efPdMOZo7ef2YPe_bTaIofMisG6AcLplKM", //- Bot's Token
+	adminId: "732304999958446111", //- Replace UserId with the Discord ID of the admin of the bot
+	token: process.env.token || "", //- Bot's Token
 	clientId: process.env.clientId || "4cb2058dfdb94bf99c2b8bf2d94b9fd3", //- ID of the bot
 	clientSecret: process.env.clientSecret || "e6b18781264f47ffbd5535bc3a2cabac", //- Client Secret of the bot
 	port: 4200, //- Port of the API and Dashboard
